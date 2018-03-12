02c93bd43a8d1b72bc10b4c66d2262e923b5aa0c
Void Chicken <VoidChicken@users.noreply.github.com>
2018-03-12 12:41:24 -0700

    added error calculation to gradient descent module

--- mod/gradientdescent.js
+++ mod/gradientdescent.js
@@ -1,1 +1,19 @@
-
+let module = new NeuralNetwork.TrainingModule(
+function()
+{
+  module.targets = [];
+},
+function(network)
+{
+  //calculate error
+  for (let i = 0; i < network.layers[network.layers.length-1].length; i++)
+  {
+    let node = network.layers[network.layers.length-1][i];
+    let result = node.value;
+    let target = module.targets[i];
+    node.error = target-result;
+  }
+  
+  //propogate error
+  
+}, "GRADIENT_DESCENT");
