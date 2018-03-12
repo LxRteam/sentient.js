50331729e6e8de4a0e6357cc1f9edc4f379e0cfe
Void Chicken <VoidChicken@users.noreply.github.com>
2018-03-12 13:01:39 -0700

    Update gradientdescent.js

--- mod/gradientdescent.js
+++ mod/gradientdescent.js
@@ -2,6 +2,7 @@ let module = new NeuralNetwork.TrainingModule(
 function()
 {
   module.targets = [];
+  module.learningRate = 0;
 },
 function(network)
 {
@@ -15,5 +16,21 @@ function(network)
   }
   
   //propogate error
+  for (let i = network.layers.length-1; i >= 1; i--)
+  {
+    for (let j = 0; j < network.layers[i].length; j++)
+    {
+      for (let k = 0; k < network.layers[i][j].weights.length; k++) {
+        network.layers[i-1][k].error = network.layers[i][j].error * network.layers[i][j].weights[k];
+        network.layers[i-1][k].weightSum += network.layers[i][j].weights[k]
+      }
+    }
+    for (let j = 0; j < network.layers[i-1].length; j++)
+    {
+      network.layers[i-1][k].error /= network.layers[i-1][k].weightSum;
+    }
+  }
+  
+  //modify weights and biases
   
 }, "GRADIENT_DESCENT");
